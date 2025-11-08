3e09af51ff3f87c8958835901c1ea4331046ddc9
Max Henry <mhenry@eers.ca>
2025-11-08 15:38:04 -0500

    add smart pointer to wwise engine as poc

--- JucePart/PluginProcessor.h
+++ JucePart/PluginProcessor.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <juce_audio_processors/juce_audio_processors.h>
+#include "GameGainFX.h"
 
 //==============================================================================
 class AudioPluginAudioProcessor final : public juce::AudioProcessor
@@ -45,4 +46,5 @@ public:
 private:
     //==============================================================================
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (AudioPluginAudioProcessor)
+    std::unique_ptr<GameGainFX> m_WwiseEffect;
 };
